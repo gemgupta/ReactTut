4a0da8cd98459cead94b2fd3b74256cd24cd4ae2
gemgupta <gemgupta007@gmail.com>
2023-08-04 17:43:21 +0530

    addingdynamicplaceholder

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,22 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
+    const expenseDate= new Date(2023,7,5);
+    const expenseDesription= "Food";
+    const expensePrice= 'Rs 10';
+    const locationOfExpen= 'Delhi';
   return (
     <div>
-      <h3>Expense Item!!!</h3>
-      <div>Food Rs 10</div>
-      <div> Petrol Rs 100</div>
-      <div>Movies Rs 200</div>
-    </div>
+      <div className="expense-item">
+        <h3>{expenseDate.toISOString()}</h3>
+        <div className="expense-item__description">
+          <h3>{expenseDesription}</h3>
+          <h3>{locationOfExpen}</h3>
+          <div className="expense-item__price"> {expensePrice}</div>
+        </div>
+      </div>
+    
+      </div>
+    
   );
 }
 export default ExpenseItem;
