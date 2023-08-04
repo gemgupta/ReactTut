426a182309e85ced951c8dfe2d338e962f8c8c44
gemgupta <gemgupta007@gmail.com>
2023-08-04 19:52:20 +0530

    UsesOfProps

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-    const expenseDate= new Date(2023,7,5);
-    const expenseDesription= "Food";
-    const expensePrice= 'Rs 10';
-    const locationOfExpen= 'Delhi';
+function ExpenseItem(props) {
+    
   return (
     <div>
       <div className="expense-item">
-        <h3>{expenseDate.toISOString()}</h3>
+        <h3>{props.date}</h3>
         <div className="expense-item__description">
-          <h3>{expenseDesription}</h3>
-          <h3>{locationOfExpen}</h3>
-          <div className="expense-item__price"> {expensePrice}</div>
+          <h3>Spent on purchasing {props.title} in {props.location}</h3>
+         
+          <div className="expense-item__price"> {props.amount}</div>
         </div>
       </div>
     
