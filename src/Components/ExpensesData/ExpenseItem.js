44b1f2333cfc50020d17dd5c9e5cf752e04c2f64
gemgupta <gemgupta007@gmail.com>
2023-08-05 16:51:42 +0530

    withRemoveButton

--- src/Components/ExpensesData/ExpenseItem.js
+++ src/Components/ExpensesData/ExpenseItem.js
@@ -1,16 +1,24 @@
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
+import Card from '../UI/Card';
+
 const ExpenseItem=(props)=> {
+  const ClickHandler= ()=>{
+    console.log('clicked!!!')
+  }
   return (
-    <div className="expense-item">
+   
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         amount={props.amount}
         location={props.location}
         title={props.title}
       />
-    </div>
+      <button onClick={ClickHandler}>Delete Expenses</button>
+    </Card>
+    
   );
 }
 export default ExpenseItem;
