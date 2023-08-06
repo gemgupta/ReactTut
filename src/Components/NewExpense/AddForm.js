99ac86351c909b0ccc8bfbdbbacad895f49d38b0
gemgupta <gemgupta007@gmail.com>
2023-08-06 11:23:57 +0530

    MultipleStateChange

--- src/Components/NewExpense/AddForm.js
+++ src/Components/NewExpense/AddForm.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddForm.css";
 const AddForm = () => {
-    const addFormHandler =(event)=>{
-console.log(event.target.value)
-    }
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const addFormHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(enteredTitle);
+  };
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const addFormHandler1 = (event) => {
+    setEnteredAmount(event.target.value);
+    console.log(enteredAmount);
+  };
+  const [enteredDate, setenteredDate] = useState("");
+  const addFormHandler2 = (event) => {
+    setenteredDate(event.target.value);
+    console.log(enteredDate);
+  };
+  const [enteredLocation, setenteredLocation] = useState("");
+  const addFormHandler3 = (event) => {
+    setenteredLocation(event.target.value);
+    console.log(enteredLocation);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -13,15 +31,25 @@ console.log(event.target.value)
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={addFormHandler}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={addFormHandler1}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2023-12-31" onChange={addFormHandler} />
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2023-12-31"
+            onChange={addFormHandler2}
+          />
         </div>
         <div className="new-expense__control">
           <label>Location</label>
-          <input type="text" onChange={addFormHandler} />
+          <input type="text" onChange={addFormHandler3} />
         </div>
       </div>
       <div className="new-expense__actions">
