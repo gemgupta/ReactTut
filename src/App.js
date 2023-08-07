44b18f4deab59da8ee5de99a85a52d8f6dcd759d
gemgupta <gemgupta007@gmail.com>
2023-08-07 13:19:36 +0530

    FilterWorking

--- src/App.js
+++ src/App.js
@@ -4,53 +4,64 @@ import Card from "./Components/UI/card";
 import ExpenseFilter from "./Components/ExpensesData/ExpenseFilter";
 import NewExpense from "./Components/NewExpense/NewExpense";
 import { useState } from "react";
-
+let expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2021, 7, 14),
+    location: "New York",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2022, 2, 12),
+    location: "Dubai",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2023, 2, 28),
+    location: "Menchester",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2023, 5, 12),
+    location: "Bangkok",
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2023, 7, 14),
-      location: "New York",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2023, 2, 12),
-      location: "Dubai",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2023, 2, 28),
-      location: "Menchester",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2023, 5, 12),
-      location: "Bangkok",
-    },
-  ];
+  
   const [expenses1, setExpense] = useState(expenses);
 
   const getDataHandler = (newExpenseData) => {
-    console.log(newExpenseData);
     setExpense((prevExpense) => {
       return [newExpenseData, ...prevExpense];
     });
   };
+
+  
+    const [filteredYear, setFilteredYear] = useState('2021');
+  
+    const filterChangeHandler = (selectedYear) => {
+      setFilteredYear(selectedYear);
+    };
+  
+    const filteredExpenses = expenses1.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear;
+    });
+  
   return (
     <div>
       <NewExpense onGetData={getDataHandler} />
       <Card className="expenses">
-        <ExpenseFilter/>
-        {expenses1.map((item) => (
-          
+        <ExpenseFilter selected={filteredYear}
+          onChangeFilter={filterChangeHandler}/>
+        {filteredExpenses.map((item) => ( 
           <ExpenseItem
           key={item.id}
             title={item.title}
--- src/Components/ExpensesData/ExpenseFilter.js
+++ src/Components/ExpensesData/ExpenseFilter.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./ExpenseFilter.css";
 
-const expenseFilter = () => {
+const expenseFilter = (props) => {
+  const dropdownHandeler=(event)=>{
+    props.onChangeFilter(event.target.value);
+  }
   return (
     <div>
       <label>Choose a year </label>
-      <select name="Year" id="Year">
-        <option value="Filter">Filter</option>
+      <select id="Year" value={props.selected} onChange={dropdownHandeler}>
+        <option value="2020">2020</option>
         <option value="2021">2021</option>
         <option value="2022">2022</option>
         <option value="2023">2023</option>
