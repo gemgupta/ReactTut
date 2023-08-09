1065e70505234818b27e1670bb47e2d9fcd7cf6a
gemgupta <gemgupta007@gmail.com>
2023-08-09 12:32:33 +0530

    ButtonStyling

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setisValid] = useState(true);
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setisValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setisValid(false);
+    } else {
+      props.onAddGoal(enteredValue);
+    }
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler} className={`${!isValid? 'invalid': ''}`}>
+      <div className={`form-control ${!isValid? 'invalid': ''}`}>
+        <label >Course Goal</label>
+        <input
+          type="text"
+          
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit" >Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
